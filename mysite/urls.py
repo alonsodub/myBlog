18eb935a3beae57fd79b7fe52178e9affff3b608
Miguel Alonso <miguel.alonso@mobica.com>
2016-04-08 12:15:25 -0500

    add post_list.html

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from mysite.views import current_datetime
 urlpatterns = [
-    url(r'^polls/',include('polls.urls')),
+    url(r'^polls/$',include('polls.urls')),
     url(r'^time/$',current_datetime),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/$', admin.site.urls),
+    url(r'^blog/$',include('blog.urls')),
 ]
